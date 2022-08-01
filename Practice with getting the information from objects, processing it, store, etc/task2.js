ca5611153e9874e45f305f27e0c7a504b075f265
RomanVladyka97 <61026536+RomanVladyka97@users.noreply.github.com>
2022-08-01 12:19:33 +0300

    Delete Practice with getting the information from objects, processing it, store, etc directory

--- Practice with getting the information from objects, processing it, store, etc/task2.js
+++
@@ -1,6 +0,0 @@
-colorLog('Задача 2', 'info');
-function isEvenArray(initialArray) {
-   return initialArray.every(element => element % 2 == 0) ? "YES" : "NO";
-}
-let some_arr = [2,4,6,8];
-console.log(isEvenArray(some_arr));
